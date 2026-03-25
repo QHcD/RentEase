f22457a4c000418373e576b6bd3fa70021f6b887
QHcD <202201226@student.polytechnic.bh>
2026-03-25 13:31:41 +0300

    Update appsettings for Azure

--- RentEase.Reporting/obj/Debug/net8.0/RentEase.Reporting.AssemblyInfo.cs
+++ RentEase.Reporting/obj/Debug/net8.0/RentEase.Reporting.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RentEase.Reporting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0be57e878d8d0ff175957006f6a5d4dc96b300bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("RentEase.Reporting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RentEase.Reporting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
